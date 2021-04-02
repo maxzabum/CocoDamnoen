b9d0e175dfeb4771939275897984cbecb04c4f17
maxzabum <maxzabum1150@hotmail.com>
2021-04-02 19:33:27 +0700

    dui
    ee

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,7 +4,7 @@ export const getUsers = async (req, res) => {
     const users = await Users.find();
     console.log(users, "sds");
     res.send(users);
-    res.status(200).json(users);
+    // res.status(200).json(users);
   } catch (err) {
     //  res.status(404).json({ messages: err.messages });
     console.log(err.messages);
