02d6c51aae0806dcc5284615673383934ced76c7
maxzabum <maxzabum1150@hotmail.com>
2021-04-17 18:35:53 +0700

    format code

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,61 @@
-import React, {useEffect, useState} from "react";
-import HomePage from './screens/HomePage/Homepage'
-import Navbar from './components/Navbar/Navbar'
-import Newspage from './screens/NewsPage/Newspage'
-import './app.css'
-import Storepage from './screens/StorePage/Storepage'
+import React, { useEffect, useState } from "react";
+import HomePage from "./screens/HomePage/Homepage";
+import Navbar from "./components/Navbar/Navbar";
+import Newspage from "./screens/NewsPage/Newspage";
+import "./app.css";
+import Storepage from "./screens/StorePage/Storepage";
 import Ourstorypage from "./screens/OurstoryPage/Ourstorypage";
 import Contactus from "./components/Footer/Contactus";
-import {Switch, Route} from 'react-router-dom'
-import DetailNew from './screens/NewsPage/DetailNewsPage/Detailnews'
+import { Switch, Route } from "react-router-dom";
+import DetailNew from "./screens/NewsPage/DetailNewsPage/Detailnews";
 
 const App = (props) => {
-    const {location} = props
-    const [width, setWindowWidth] = useState(0)
-    const [active, setActive] = useState(false)
-    useEffect(() => {
+  const { location } = props;
+  const [width, setWindowWidth] = useState(0);
+  const [active, setActive] = useState(false);
+  useEffect(() => {
+    updateDimensions();
+    // console.log(width)
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
+  const updateDimensions = () => {
+    const width = window.innerWidth;
+    setWindowWidth(width);
+  };
+  return (
+    <div className="App">
+      <Navbar active={active} setActive={setActive} screenWidth={width} />
+      <div
+        onClick={() => {
+          setActive(false);
+        }}
+      >
+        <Switch location={location}>
+          <Route exact path="/">
+            <HomePage screenWidth={width} />
+          </Route>
+          <Route path="/ourstory">
+            <Ourstorypage screenWidth={width} />
+          </Route>
+          <Route
+            // exact
+            path="/news"
+          >
+            <Newspage screenWidth={width} />
+          </Route>
+          <Route path="/store">
+            <Storepage screenWidth={width} />
+          </Route>
 
-        updateDimensions();
-        // console.log(width)
-        window.addEventListener('resize', updateDimensions);
-        return () =>
-            window.removeEventListener('resize', updateDimensions);
-
-    }, [])
-    const updateDimensions = () => {
-        const width = window.innerWidth
-        setWindowWidth(width)
-    }
-    return (
-        <div className="App">
-            <Navbar active={active} setActive={setActive} screenWidth={width}/>
-            <div onClick={() => {
-                setActive(false)
-            }}>
-                <Switch location={location}>
-                    <Route
-                        exact
-                        path="/"
-                    >
-                        <HomePage screenWidth={width}/>
-                    </Route>
-                    <Route
-                        path="/ourstory"
-                    >
-                        <Ourstorypage screenWidth={width}/>
-                    </Route>
-                    <Route
-                        // exact
-                        path="/news"
-                    >
-                        <Newspage screenWidth={width}/>
-                    </Route>
-                    <Route
-                        path="/store"
-                        
-                    >
-                        <Storepage screenWidth={width}/>
-                    </Route>
-
-                    <Route
-                        component={DetailNew}
-                    />
-                    {/* <Ourstorypage/> */}
-                    {/* <Newspage/> */}
-                    {/* <Storepage/> */}
-                </Switch>
-                <Contactus/>
-            </div>
-
-        </div>
-    );
+          <Route component={DetailNew} />
+          {/* <Ourstorypage/> */}
+          {/* <Newspage/> */}
+          {/* <Storepage/> */}
+        </Switch>
+        <Contactus />
+      </div>
+    </div>
+  );
 };
 export default App;
--- client/src/components/Footer/Contactus.js
+++ client/src/components/Footer/Contactus.js
@@ -1,43 +1,41 @@
-import React from 'react'
-import "./contactStyles.css"
-import FBIcon from './fb.png'
-import IGIcon from './instragram.png'
-import LineIcon from './line.png'
-import TwiiterIcon from './twitter.png'
-const Contactus =()=>{
-    return(
-        <div className="contactus-container">
-            <div className="item-contactus">
-                <div className="contact-detail">
-                    <div className="left-container">
-                        <p>CONTACT US</p>
-                        <p>377/1 Moo 17, Tumbol Sila, Amphur Muang, Khon Kaen, 
-                            Thailand 40000 </p>
-                        <p>(+66) 084-204-5999 </p>
-                        <p>sales@cocodamnoen.com</p>
-                    </div>
-                    <div className="right-container">
-
-                        <div className="email-subscribe-container">
-                            <p>NEWSLETTER</p>
-                            <input type="text" placeholder="EMAIL ADDRESS"/>
-                            <button>SUBSCRIBE</button>
-                        </div>
-                        <div className="follow-buttons-container">
-                            <img src={FBIcon} alt="img"/>
-                            <img src={IGIcon} alt="img"/>
-                            <img src={LineIcon} alt="img"/>
-                            <img src={TwiiterIcon} alt="img"/>
-                        </div>
-                    </div>
-                </div>
-                <div className="copyright">
-                    <p>© Riseplus Technology. All Rights Reserved 2018.</p>
-                </div>
+import React from "react";
+import "./contactStyles.css";
+import FBIcon from "./fb.png";
+import IGIcon from "./instragram.png";
+import LineIcon from "./line.png";
+import TwiiterIcon from "./twitter.png";
+const Contactus = () => {
+  return (
+    <div className="contactus-container">
+      <div className="item-contactus">
+        <div className="contact-detail">
+          <div className="left-container">
+            <p>CONTACT US</p>
+            <p>
+              377/1 Moo 17, Tumbol Sila, Amphur Muang, Khon Kaen, Thailand 40000{" "}
+            </p>
+            <p>(+66) 084-204-5999 </p>
+            <p>sales@cocodamnoen.com</p>
+          </div>
+          <div className="right-container">
+            <div className="email-subscribe-container">
+              <p>NEWSLETTER</p>
+              <input type="text" placeholder="EMAIL ADDRESS" />
+              <button>SUBSCRIBE</button>
             </div>
-            
-            
+            <div className="follow-buttons-container">
+              <img src={FBIcon} alt="img" />
+              <img src={IGIcon} alt="img" />
+              <img src={LineIcon} alt="img" />
+              <img src={TwiiterIcon} alt="img" />
+            </div>
+          </div>
         </div>
-    )
-}
-export default Contactus
+        <div className="copyright">
+          <p>© Riseplus Technology. All Rights Reserved 2018.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Contactus;
