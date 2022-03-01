8ac5ffcb069535e03db6d07c29544f32882c0860
maxzabum <maxzabum1150@hotmail.com>
2022-03-01 19:59:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -56,6 +56,9 @@ app.use("/api/v1/store", storeRoute);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 // PORT
